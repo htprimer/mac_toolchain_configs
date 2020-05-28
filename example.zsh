c7b314ef8096657efe0027f93ecd3e09d48556b6
John <htprimer@gmail.com>
2020-05-28 03:33:52 -0400

    support lower zsh version & optimize

--- example.zsh
+++ example.zsh
@@ -10,8 +10,6 @@
 #
 
 # zsh config
-ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
-zshaddhistory() { whence ${${(z)1}[1]} >| /dev/null || return 1 }
 
 alias axcbuild='osascript -e "tell application \"Xcode\" to build active workspace document"'
 alias axcrun='osascript -e "tell application \"Xcode\" to run active workspace document"'
--- myStrategy.zsh
+++ myStrategy.zsh
@@ -1,5 +1,5 @@
 #config
-MY_ZSH_STRATEGY_VERSION=1.7
+MY_ZSH_STRATEGY_VERSION=1.8
 MY_ZSH_STRATEGY_ADDR="https://raw.githubusercontent.com/htprimer/mac_toolchain_configs/master/myStrategy.zsh"
 
 _zsh_my_strategy_check() {
@@ -21,7 +21,7 @@ zle -N my_strategy_choose_hint_up _zsh_my_strategy_choose_hint_up
 zle -N my_strategy_accept_hint _zsh_my_strategy_accept_hint
 
 _zsh_autosuggest_strategy_my_default() {
-    # Reset options to defaults and enable LOCAL_OPTIONS
+    # Reset options to defaults and enable typeset_OPTIONS
     emulate -L zsh
 
     # Enable globbing flags so that we can use (#m)
@@ -31,27 +31,29 @@ _zsh_autosuggest_strategy_my_default() {
 	# this string as a pattern to search the $history associative array.
 	# - (#m) globbing flag enables setting references for match data
 	# TODO: Use (b) flag when we can drop support for zsh older than v5.0.8
-    local prefix="${1//(#m)[\\*?[\]<>()|^~#]/\\$MATCH}"
+    typeset prefix="${1//(#m)[\\*?[\]<>()|^~#]/\\$MATCH}"
 
-    typeset -g suggestion=""
-    typeset -g all_match_hint=()
+    typeset -g suggestion
+    suggestion=''
+    typeset -g all_match_hint
+    all_match_hint=()
     typeset -g current_line_index=0
-    local input_array=(${(z)prefix})
+    typeset -a input_array
+    input_array=($(echo $prefix))
 
-    if [[ "$input_array[1]" == 'cd' ]] {
+    if [[ $input_array[1] == 'cd' ]] {
         _zsh_my_strategy_cd_complete $input_array[2]  #cd 补全提示
-    } elif [[ "$input_array[1]" == 'git' && "$input_array[2]" == 'checkout' ]] {
+    } elif [[ $input_array[1] == 'git' && $input_array[2] == 'checkout' ]] {
         _zsh_my_strategy_git_complete $input_array[3] #切分支补全
     } else {
-        local hint=${history[(r)${prefix}*]}  #history是一个哈希表
-        local hint_word=(${(z)hint})
+        typeset hint=${history[(r)${prefix}*]}  #history是一个哈希表
 
-        if [[ "$hint_word[1]" == 'cd' ]] {
+        if [[ ${hint[(w)1]} == 'cd' ]] {
             _zsh_my_strategy_cd_complete   
-        } elif [[ "$hint_word[1]" == 'git' && "$hint_word[2]" == 'checkout' ]] {
+        } elif [[ ${hint[(w)1]} == 'git' && ${hint[(w)2]} == 'checkout' ]] {
             _zsh_my_strategy_git_complete
         } else {
-            [ -n "$(whence $hint_word[1])" ] && suggestion="$hint"  #过滤无效记录
+            [ -n "$(whence ${hint[(w)1]})" ] && suggestion="$hint"  #过滤无效记录
             if (( $#hint )) {
                 all_match_hint=(${(u)history[(R)$prefix*]})
                 all_match_hint=(${all_match_hint:#$hint})
@@ -72,19 +74,34 @@ _zsh_autosuggest_strategy_my_default() {
 }
 
 _zsh_my_strategy_git_complete() {
-    local git=$(git status 2>&1)
+    typeset git
+    git=$(git status 2>&1)
     if [[ "$git" == 'fatal'* ]] {
         return
     }
-    local -A branchs=()
-    for origin_branch ($(git branch -r)) {
-        local branch_name=${origin_branch#'origin/'}
-        if [[ $branch_name != 'HEAD'* ]] && [[ $branch_name != '->' ]] {
+    typeset -A branchs
+    # typeset -a todo_branchs
+    # todo_branchs=($(git branch -r))
+    #性能优化 远端分支数量太多则不匹配
+    # if (( $#todo_branchs > 1000 )) {
+    for branch_name ($(git branch)) {
+        if [[ $branch_name != '*' ]] {
             branchs[$branch_name]=$branch_name
         }
     }
+    # }
+    #  else {
+    #     for origin_branch ($todo_branchs) {
+    #         # local branch_name=${origin_branch#'origin/'}
+    #         typeset branch_name=${origin_branch:7}
+    #         if [[ $branch_name != 'HEAD'* ]] && [[ $branch_name != '->' ]] {
+    #             branchs[$branch_name]=$branch_name
+    #         }
+    #     }
+    # } 
     if (( $#1 >0 )) {
-        local match_branchs=(${branchs[(R)$1*]})
+        typeset -a match_branchs
+        match_branchs=(${branchs[(R)$1*]})
         for branch_name ($match_branchs) {
             all_match_hint+="git checkout $branch_name"
         }
@@ -100,12 +117,12 @@ _zsh_my_strategy_git_complete() {
 }
 
 _zsh_my_strategy_cd_complete() {
-    local -A dirs=()  #用关联数组，R下标标志返回所有结果
+    typeset -A dirs  #用关联数组，R下标标志返回所有结果
     if (( $#1 > 0)) {
-        local arg=$1
-        local whole_arg=$arg
-        local slash_index=$arg[(I)/]
-        local dir=''
+        typeset arg=$1
+        typeset whole_arg=$arg
+        typeset slash_index=$arg[(I)/]
+        typeset dir=''
         if (( $slash_index > 0 && $slash_index != 1 )) {
             arg=$whole_arg[(($slash_index+1)),$#whole_arg]
             dir=$whole_arg[1,$slash_index]
@@ -123,14 +140,16 @@ _zsh_my_strategy_cd_complete() {
                 }
             }
         } else {
-            local items=($(ls))
+            typeset -a items
+            items=($(ls))
             if (( $#items )) {  #判断是否空目录 避免*号报错
                 for file (*) {
                     [[ -d $file ]] && dirs[$file]=$file 
                 }
             }
         }
-        local match_dirs=(${dirs[(R)$dir$arg*]})  #查找当前目录
+        typeset -a match_dirs #查找当前目录
+        match_dirs=(${dirs[(R)$dir$arg*]}) 
         if (( $#match_dirs <= 0 )) {
             arg=${(U)arg} #转成大写尝试
             match_dirs=(${dirs[(R)$dir$arg*]})
@@ -149,7 +168,8 @@ _zsh_my_strategy_cd_complete() {
             all_match_hint=($all_match_hint[1,10])
         }
     } else {
-        local items=($(ls))
+        typeset -a items
+        items=($(ls))
         if (( $#items )) {
             for file (*) {
                 if [[ -d $file ]] {
@@ -206,14 +226,14 @@ _zsh_autosuggest_highlight_apply() {
 		region_highlight+=("$_ZSH_AUTOSUGGEST_LAST_HIGHLIGHT")
         
         if (( $current_line_index > 0 )) {
-            local line=$'\n'
-            local current_line_start=$(($#BUFFER + $#right_hint + $#line))
+            typeset line=$'\n'
+            typeset current_line_start=$(($#BUFFER + $#right_hint + $#line))
             for i ($all_match_hint[1,$((current_line_index-1))]) {
                 (( current_line_start+=$#i ))
                 (( current_line_start+=$#line ))
             }
-            local current_line=$all_match_hint[$current_line_index]
-            local current_hint_highlight="$current_line_start $(($current_line_start + $#current_line)) fg=209,bg=8,bold"
+            typeset current_line=$all_match_hint[$current_line_index]
+            typeset current_hint_highlight="$current_line_start $(($current_line_start + $#current_line)) fg=209,bg=8,bold"
             region_highlight+=("$current_hint_highlight")
         }
 	else
@@ -223,11 +243,11 @@ _zsh_autosuggest_highlight_apply() {
 
 #重写显示方法
 _zsh_autosuggest_suggest() {
-    local suggestion="$1"
+    typeset suggestion="$1"
 
     if [[ -n "$suggestion" ]] && (( $#BUFFER )); then
         typeset -g right_hint="${suggestion#$BUFFER}"
-        local all_hint=$'\n'"$(print -l $all_match_hint)"
+        typeset all_hint=$'\n'"$(print -l $all_match_hint)"
         POSTDISPLAY="$right_hint$all_hint"
         # POSTDISPLAY="${suggestion#$BUFFER}"
     elif (( $#all_match_hint )); then
@@ -242,7 +262,7 @@ _zsh_autosuggest_suggest() {
 
 #重写接受方法
 _zsh_autosuggest_accept() {
-    local -i max_cursor_pos=$#BUFFER
+    typeset -i max_cursor_pos=$#BUFFER
 
     # When vicmd keymap is active, the cursor can't move all the way
     # to the end of the buffer
@@ -274,14 +294,14 @@ _zsh_autosuggest_accept() {
 
 #重写modify方法
 _zsh_autosuggest_modify() {
-    local -i retval
+    typeset -i retval
 
     # Only available in zsh >= 5.4
-    local -i KEYS_QUEUED_COUNT
+    typeset -i KEYS_QUEUED_COUNT
 
     # Save the contents of the buffer/postdisplay
-    local orig_buffer="$BUFFER"
-    local orig_postdisplay="$POSTDISPLAY"
+    typeset orig_buffer="$BUFFER"
+    typeset orig_postdisplay="$POSTDISPLAY"
 
     # Clear suggestion while waiting for next one
     unset POSTDISPLAY
@@ -297,7 +317,7 @@ _zsh_autosuggest_modify() {
 
     # Optimize if manually typing in the suggestion
     if (( $#BUFFER > $#orig_buffer )); then
-        local added=${BUFFER#$orig_buffer}
+        typeset added=${BUFFER#$orig_buffer}
 
         # If the string added matches the beginning of the postdisplay
         if [[ "$added" = "${orig_postdisplay:0:$#added}" ]]; then
